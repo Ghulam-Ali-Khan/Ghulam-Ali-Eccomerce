4fcad97b151585028fa765bfa7e663c377c7f3a5
Ghulam Ali <ghulam.ali@netrootstech.com>
2023-06-14 01:37:09 +0500

    Footer Done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,71 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography, Grid, Container } from '@mui/material'
+import { AppBar, Toolbar, Typography, Grid, Container, List, ListItem, Button } from '@mui/material';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import GoogleIcon from '@mui/icons-material/Google';
+
+
+
 const Footer = () => {
+
+    const quick_links = ['Privacy Policy', 'Terms and Conditions', 'Need Help', 'Contact Us', 'About Us'];
+
+
     return (
         <>
-            <AppBar position="static" className='footer-top' >
+
+            <AppBar position="static" className='footer-top'  >
                 <Toolbar>
 
-                <Container maxWidth="xl">
-                    <Grid container spacing={3}>
+                    <Container maxWidth="xl" >
+                        <Grid container spacing={3}>
 
-                        <Grid item xs={4}>
-                            <Typography>Column 1</Typography>
+                            <Grid item lg={3} xs={12} className='footer-col'>
+                                <img src="/src/imgs/Sample-footer-logo.png" alt="logo" className='logo' />
+                                <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta, rerum vitae dolorum adipisci quia deserunt incidunt! Incidunt amet sapiente totam? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt fugit iusto suscipit amet asperiores inventore, expedita maxime ab! Unde, ipsa?</Typography>
+                                <div className="social-icons">
+                                    <GoogleIcon className='rounded' />
+                                    <PinterestIcon />
+                                    <InstagramIcon className='rounded' />
+                                    <FacebookRoundedIcon />
+                                </div>
+                            </Grid>
+                            <Grid item lg={3} xs={12} className='footer-col'>
+                                <Typography className='footer-links-head' component='h2'>Quick Links</Typography>
+                                <List className='links'>
+                                    {
+                                        quick_links.map((item, index) => (
+
+                                            <ListItem key={index} className='link'>{item}</ListItem>
+
+                                        ))
+                                    }
+                                </List>
+                            </Grid>
+                            <Grid item lg={3} xs={12} className='footer-col'>
+                                <Typography className='footer-links-head' component='h2'>Popular Categories</Typography>
+                                <List className='links'>
+                                    {
+                                        quick_links.map((item, index) => (
+
+                                            <ListItem key={index} className='link'>{item}</ListItem>
+
+                                        ))
+                                    }
+                                </List>
+                            </Grid>
+                            <Grid item lg={3} xs={12} className='footer-col subscribe-col'>
+                                <Typography className='footer-links-head' component='h2'>Subscribe</Typography>
+                                <Typography component='p'>Subscribe to up-to-date yourself with our latest offers.</Typography>
+                                <input type="email" className='subscribe-field' placeholder='Email'/>
+                                <Button variant='contained' className='subscribe-btn'>Subscribe</Button>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={4}>
-                            <Typography>Column 2</Typography>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Typography>Column 3</Typography>
-                        </Grid>
-                    </Grid>
                     </Container>
                 </Toolbar>
             </AppBar>
+
         </>
     )
 }
